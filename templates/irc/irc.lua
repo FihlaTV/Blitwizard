c84785c3eafba05cb5cd2aa5500d938861beec05
Jonas Thiem <jonasthiem@googlemail.com>
2013-05-04 18:45:25 +0200

    IRC code and documentation update

--- templates/irc/irc.lua
+++ templates/irc/irc.lua
@@ -17,103 +17,33 @@ Permission is granted to anyone to use this software for any purpose, including 
 ]]
 
 --[[--
+ This is the IRC module which allows you to connect to the popular
+ <a href="http://en.wikipedia.org/wiki/IRC">Internet Relay Chat</a>.
+
+ You can add ingame chat rooms to your game quite easily using this
+ module.
  @module blitwizard.net.irc
 ]]
 
+
 blitwizard.net.irc = {}
 
-blitwizard.net.irc.open = function(name, port, nickname, callback_on_event)
+--[[--
+ This is the IRC connection class type which embodies an IRC server
+  connection.
+
+ You can create as many as you like if you want to connect to
+ multiple servers at once.
+ @type connection
+]]
+blitwizard.net.irc.connection = {}
+
+function blitwizard.net.irc.connection:new(name, callback_on_event,
+nickname, port)
     --  
     --  *** Blitwizard IRC interface ***
     --
-    --  Read on for documentation.
-    --
-    -- This opens a new blitwizard IRC interface instance to an IRC server
-    -- (one instance can handle one server). It returns an IRC object.
-    --
-    -- callback_on_event needs to be a lua function provided by you which
-    -- gets called with event infos as soon as something happens.
-    --
-    -- callback_on_event gets the irc object as first parameter,
-    -- an event name as a second parameter,
-    -- and additional parameters depending on the event type.
-    --
-    -- Once you get the "connect" event, use joinChannel to enter chat rooms
-    -- (=channels).
-    --
-    -- Event types:
-    --     "connect": parameters: 
-    --                The given IRC instance has connected to the server.
-    --                Now you might want to use
-    --                irc:joinCannel("#somechannel")
-    --     "join": parameters: channel, nickname
-    --             Someone with the given nickname joined the given channel.
-    --             If it is yourself, you'll get a "names" event shortly after.
-    --             You can use this to speak in a channel you joined:
-    --               irc:talkToChannel(channel, "hello")
-    --             You can obtain a list of users like this:
-    --               irc:getChannelUsers(channel)
-    --     "leave": parameters: channel, nickname, reason
-    --              Someone with the given nickname left the given channel.
-    --              The reason can contain a free text string specifying
-    --              a reason.
-    --              This can also be yourself.
-    --              You might want to update your user list with
-    --                irc:getChannelUsers(channel)
-    --     "quit": parameters: nickname, reason
-    --             Someone with the given nickname disconnected (-> leaves all
-    --             channels). The reason can contain a free text string
-    --             specifying a reason.
-    --             You might want to update your user list with:
-    --                irc:getChannelUsers(channel)
-    --     "channelTalk": parameters: channel, nickname, message
-    --                       Someone said something in a channel.
-    --                       message is a string with the message.
-    --                       This should show up e.g. like that:
-    --                         <nickname> hello
-    --                       (with "hello" being the message)
-    --     "channelAction": same as channel message, but indicates it is a
-    --                      /me action of the user
-    --                      This should show up e.g. like that:
-    --                        <nickname does something>
-    --                      (with "does something" being the message)
-    --     "privateTalk": parameters: nickname, message
-    --                       Someone said something privately to you.
-    --                       You might want to respond with
-    --                       irc:talkToPerson(nickname, "hello friend")
-    --     "privateAction": same as private message, but indiciates it is a
-    --                      /me action of the user
-    --                      (see channelAction)
-    --     "disconnect": parameters: irc
-    --                     You were disconnected from the server.
-    --                     You should use irc:quit() and leave the irc object
-    --                     alone.
-    --
-    --  Additionally, you might be interested in those functions sitting
-    --  on the IRC object returned:
-    --
-    --   irc:joinChannel(channel)
-    --     Join the channel named by the specific string.
-    --   irc:leaveChannel(channel)
-    --     Leave the specified channel again.
-    --   irc:talkToChannel(channel, message)
-    --     Talk in a given channel. Please note on most servers,
-    --     you need to be inside the channel to do that.
-    --   irc:talkToPerson(channel, nickname)
-    --     Talk to a person with the given nickname in private.
-    --     Please note offline messages are not supported
-    --     (the message won't go anywhere if that person is offline)
-    --   irc:getChannelUsers(channel)
-    --     Return an array of strings with the names of all users
-    --     inside the given channel.
-    --     Please note this will error if you're not inside the
-    --     specified cannel.
-    --   irc:getNickname(server)
-    --    Get the nickname you have on the given server/blitwizard IRC instance
-    --   irc:quit(server, reason)
-    --    Leave the server with the specified reason (a text message/string,
-    --    e.g. "Goodbye")
-    --
+    -- Check the generated documentation for usage.
 
     local server = {
         connection = "",
@@ -123,16 +53,32 @@ blitwizard.net.irc.open = function(name, port, nickname, callback_on_event)
 
     -- enforce proper server name:
     if (type(name) ~= "string") then
-        error("bad argument #1 to blitwizard.net.irc.open: " ..
+        error("bad argument #1 to blitwizard.net.irc.connection:new: " ..
         "provided server name is not a string")
+    end
+
+    if port == nil or type(port) ~= "number" or port < 1 or port > 33000 then
+        port = 6667
+    end
+    port = math.floor(port)
+
+    if type(callback_on_event) ~= "function" then
+        error("bad argument #2 to blitwizard.net.irc.connection:new: " ..
+        "provided callback function is not a function")
+    end
+
+    if type(nickname) ~= "string" then
+        error("bad argument #3 to blitwizard.net.irc.connection:new: " ..
+        "provided nickname is not a string")
     end
 
     -- set proper meta table settings:
     local metatb = {
-        __index = blitwizard.net.irc
+        __index = blitwizard.net.irc.connection
     }
     setmetatable(server, metatb)
 
+    server.nickname = nickname
     server.connection = 
     blitwizard.net.open({server=name, port=port, linebuffered=true},
         function(stream)
@@ -184,11 +130,23 @@ blitwizard.net.irc.open = function(name, port, nickname, callback_on_event)
             if string.starts(args[1], ":") and #args >= 2 then
                 local actor = string.split(string.sub(args[1], 2), "!", 1)
                 if args[2] == "001" then
+                    server.connected = true
                     callback_on_event(server, "connect")
+                end
+                if args[2] == "366" then
+                    -- end of names for channel args[4]
+                    if server.channels[args[4]] then
+                        if server.channels[args[4]].joinDelayed then
+                            -- fire delayed join event
+                            server.channels[args[4]].joinDelayed = false
+                            callback_on_event(server, "enter",
+                            args[4], server.nickname)
+                        end
+                    end
                 end
                 if string.lower(args[2]) == "join" then
                     -- remember we are inside this channel if this is us:
-                    if string.lower(actor) == string.lower(self.nickname) then
+                    if string.lower(actor) == string.lower(server.nickname) then
                         if not server.channels[args[3]] then
                             server.channels[args[3]] = {}
                             server.channels[args[3]].joinDelayed = true
@@ -198,7 +156,22 @@ blitwizard.net.irc.open = function(name, port, nickname, callback_on_event)
                         -- the NAMES info first so we can present a user list.
                     else
                         -- a regular join by someone else:
-                        callback_on_event(server, "join", args[3], actor)
+                        if server.channels[args[3]] ~= nil then
+                            if not server.channels[args[3]].joinDelayed
+                            then
+                                -- we are inside the room! -> deliver event
+                                callback_on_event(server, "enter",
+                                args[3], actor)
+                            else
+                                -- channel join was delayed (waiting for
+                                -- NAMES).
+                                -- store event for now:
+                                local events = server.channels[args[3]].
+                                    delayedEvents
+                                events[#events+1] = { "enter", args[3],
+                                actor }
+                            end
+                        end
                     end
                 end
                 if string.lower(args[2]) == "privmsg" then
@@ -214,8 +187,22 @@ blitwizard.net.irc.open = function(name, port, nickname, callback_on_event)
                             else
                                 args[4] = ""
                             end
-                            callback_on_event(server, "channelAction",
-                            args[3], actor, args[4])
+                            if server.channels[args[3]] ~= nil then
+                                if not server.channels[args[3]].joinDelayed
+                                then
+                                    -- we are inside the room!
+                                    callback_on_event(server, "roomAction",
+                                    args[3], actor, args[4])
+                                else
+                                    -- channel join was delayed (waiting for
+                                    -- NAMES).
+                                    -- store event for now:
+                                    local events = server.channels[args[3]].
+                                        delayedEvents
+                                    events[#events+1] = { "roomAction", args[3],
+                                    actor, args[4] }
+                                end
+                            end
                             return
                         end
                     end
@@ -223,14 +210,14 @@ blitwizard.net.irc.open = function(name, port, nickname, callback_on_event)
                     if server.channels[args[3]] ~= nil then
                         if not server.channels[args[3]].joinDelayed then
                             -- we're on that channel! send talk event:
-                            callback_on_event(server, "channelTalk", args[3],
+                            callback_on_event(server, "roomTalk", args[3],
                             actor, args[4])
                         else
                             -- channel join is delayed (to wait for NAMES).
                             -- store event to emit it later:
                             local events = server.channels[args[3]].
                                 delayedEvents
-                            events[#events+1] = { "channelTalk", args[3],   
+                            events[#events+1] = { "roomTalk", args[3],   
                             actor, args[4] }
                         end
                     end
@@ -247,7 +234,50 @@ blitwizard.net.irc.open = function(name, port, nickname, callback_on_event)
     return server
 end
 
-function blitwizard.net.irc:quit(reason)
+--[[--
+  Create a new IRC instance. (see usage example below!)
+  @function new
+  @tparam string server the address of the server, e.g. irc.eloxoph.de
+  @tparam function callback the event callback. It will be called with first parameter being this IRC instance, the second one the event name and all additional parameters depending on the event.
+
+  The following events are known:
+
+  "connect": the connection was successfully established. Use @{blitwizard.net.irc.connection:enterRoom|connection:enterRoom} to enter a chat room now!
+
+  "enter": parameters: room, user name. The user with the given name entered the specified chat room. <b>If the user name is yours (@{blitwizard.net.irc.connection:getUsername|connection:getUsername}), this means you are inside the chat room now</b> and you may participate in conversations using @{blitwizard.net.irc.connection:talkToRoom|connection:talkToRoom} or @{blitwizard.net.irc.connection:doRoomAction|connection:doRoomAction}.
+
+  @tparam number username the desired user name to be used on the chat server
+  @tparam port (optional) specify the server port. Defaults to 6667, the most commonly used IRC server port
+  @usage
+  -- connect to the #blitwizard chat room:
+  myIRC = blitwizard.net.irc.connection:new("irc.eloxoph.de",
+  function(self, event, param1, param2, param3)
+      if event == "connect" then
+          print("Connected! Will now enter chat room...")
+          self:enterRoom("#blitwizard")
+      elseif event == "enter" then
+          if string.lower(param1) == "#blitwizard" and
+          param2 == self:getUsername() then
+              print("Now inside #blitwizard! Say \"Hello World!\"...")
+              self:talkToRoom("#blitwizard", "Hello World!")
+          end
+       elseif event == "roomTalk" and
+        string.lower(param1) == "#blitwizard" then
+           -- a person talked in #blitwizard! display it!
+           print("<" .. param2 .. "> " .. param3)
+       elseif event == "roomAction" and
+        string.lower(param1) == "#blitwizard" then
+           -- a person did an 'action' in #blitwizard. display it:
+           print("<" .. param2 .. " " .. param3 .. ">")
+       end
+  end, "testUser")
+]]
+
+--[[--
+  Close an IRC connection.
+  @function close
+]]
+function blitwizard.net.irc.connection:quit(reason)
     if reason == nil then
         reason = "Bye"
     end
@@ -256,29 +286,90 @@ function blitwizard.net.irc:quit(reason)
     self.connection = nil
 end
 
-function blitwizard.net.irc:getNickname(server)
+--[[--
+   Get the user name currently used on the chat server.
+   @function getUsername
+   @treturn string the user name currently used
+]]
+function blitwizard.net.irc.connection:getUsername()
     return self.nickname
 end
 
-function blitwizard.net.irc:joinChannel(channel)
+--[[--
+   Join a specific chat room
+   @function enterRoom
+   @tparam string room the name of the chat room. Please note that on most servers, the name is required to start with #. Space chars are not allowed.
+]]
+function blitwizard.net.irc.connection:enterRoom(channel)
+    if self.connected ~= true then
+        error("not connected to server")
+    end
+    if channel == nil then
+        error("bad argument #1 to blitwizard.net.irc.connection:enterRoom: " ..
+        "string expected")
+    end
+    channel = channel:gsub("\n", " ")
+    channel = channel:gsub("\r", " ")
+    channel = string.split(channel, " ", 1)
+    channel = channel:gsub(",", "")
     blitwizard.net.send(self["connection"], "JOIN " .. channel .. "\n")
 end
 
-function blitwizard.net.irc:talkToPerson(nickname, msg)
+--[[--
+   Talk to a specific person (in private chat).
+
+   Please note offline chat is not supported (message will not arrive
+   if user is offline!)
+   @function talkToPerson
+   @tparam string username the user name of the person to talk to
+   @tparam string msg the message you want to send to the person
+]]
+function blitwizard.net.irc.connection:talkToPerson(nickname, msg)
     blitwizard.net.send(self["connection"],
     "PRIVMSG " .. nickname .. " :" .. string.split(
         string.split(msg, "\n", 2)[1], "\r", 2)[1] .. "\n")
 end
 
-function blitwizard.net.irc:talkToChannel(channel, msg)
-    blitwizard.net.send(self["connection"], "PRIVMSG " .. channel .. " :" ..
-    string.split(string.split(msg, "\n", 2)[1], "\r", 2)[1] .. "\n")
+--[[--
+   Talk to a specific chat room. Please note this is not possible
+   before the "enter" event!
+   @function talkToRoom
+   @tparam string room the chat room to talk to
+   @tparam string msg the message you want to send
+]]
+function blitwizard.net.irc.connection:talkToRoom(channel, msg)
+    if self.channels[channel] ~= nil then
+        if not self.channels[channel].joinDelayed then
+            blitwizard.net.send(self["connection"], "PRIVMSG " .. channel ..
+            " :" ..
+            string.split(string.split(msg, "\n", 2), "\r", 2) .. "\n")
+            return
+        end
+    end
+    error("you are not inside chat room " .. channel)
 end
 
-function blitwizard.net.irc:doChannelAction(channel, msg)
-    blitwizard.net.send(self.connection, "PRIVMSG " .. channel ..
-    " :\001ACTION " .. string.split(string.split(msg, "\n", 2)[1], "\r", 2)[1]
-     .. "\001\n")
+--[[--
+   Roleplay a specific action in a room, e.g. "looks outside of window"
+   as an action will show up as "<Yourname> looks outside the window".
+
+   Similar to @{blitwizard.net.irc.connection:talkToRoom|connection.talkToRoom}, this is not
+   possible before entering a room and getting the "enter" event.
+   @function doRoomAction
+   @tparam string room the chat room you want to do the action in
+   @tparam string action the description of your action
+]]
+function blitwizard.net.irc.connection:doRoomAction(channel, msg)
+    if self.channels[channel] ~= nil then
+        if not self.channels[channel].joinDelayed then
+            blitwizard.net.send(self["connection"], "PRIVMSG " .. channel ..
+            " :\001ACTION " ..
+            string.split(string.split(msg, "\n", 2), "\r", 2) ..
+            "\001\n")
+            return
+        end
+    end
+    error("you are not inside chat room " .. channel)
 end
 
 
