0125a142c03f3987d88b431acdea1aee2a1de22f
Valentin Ochs <a@0au.de>
2013-09-07 23:50:55 +0200

    tabs -> 4 spaces

--- templates/init.lua
+++ templates/init.lua
@@ -1,30 +1,30 @@
 
 -- Avoid double initialisation
 if blitwizard.templatesinitialised == true then
-	return
+    return
 end
 blitwizard.templatesinitialised = true
 
 -- Load all the templates
 if os.sysname() ~= "Android" then
-	-- Crawl the templates/ folder for templates
-	local templates = os.templatedir()
+    -- Crawl the templates/ folder for templates
+    local templates = os.templatedir()
     if templates == nil then
         os.forcetemplatedir("templates/")
         templates = "templates/"
     end
-	for index,file in ipairs(os.ls(templates)) do
-		if os.isdir(templates .. "/" .. file) then
-			local filepath = templates .. "/" .. file .. "/" .. file .. ".lua"
-			if os.exists(filepath) then
-				dofile(filepath)
-			end
-		end
-	end
+    for index,file in ipairs(os.ls(templates)) do
+        if os.isdir(templates .. "/" .. file) then
+            local filepath = templates .. "/" .. file .. "/" .. file .. ".lua"
+            if os.exists(filepath) then
+                dofile(filepath)
+            end
+        end
+    end
 else
-	-- For android, we get a pre-generated Lua file
+    -- For android, we get a pre-generated Lua file
     -- (provided by the android build script in scripts/ folder
     -- which assembled the .apk package)
-	dofile("templates/filelist.lua")
+    dofile("templates/filelist.lua")
 end
 
